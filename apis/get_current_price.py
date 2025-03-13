a6e7410afe2df8a5aebd1ada2076ebc58781a79a
Shiv Vinny Bhutani <shivvinnybhutani@MacBook-Pro.local>
2025-03-13 07:56:43 -0400

    timezone issues

--- apis/get_current_price.py
+++ apis/get_current_price.py
@@ -10,4 +10,9 @@ ALPHA_ADVANTAGE_API_KEY = os.getenv("ALPHA_ADVANTAGE_API_KEY")
 def get_current_price(stockSymbol):
     eastern = pytz.timezone('US/Eastern')
     recent_data = yfinance.download(stockSymbol,period = "1d")
-    return (recent_data.loc[datetime.now(eastern).date().strftime("%Y-%m-%d")])
+    #fallback_data = yfinance.download(stockSymbol,)
+    #data = recent_data.loc[datetime.now(eastern).date().strftime("%Y-%m-%d")]
+    data = recent_data.tail(1)
+    return data
+
+
